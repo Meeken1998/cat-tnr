e9a746508149cbe2e3e431f800f1bc9dce0b1b8e
Meeken <78071836@qq.com>
2020-05-29 21:10:49 +0800

    feat: 猫咪图鉴页面

---
+++ component/cat-item/index.js
@@ -0,0 +1,17 @@
+// component/cat-item/index.js
+Component({
+  properties: {
+    options: {
+      type: Object,
+      value: {
+        photo: 'https://dss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=1917461690,696184102&fm=5',
+        name: '小明'
+      }
+    },
+
+    border: {
+      type: Boolean,
+      value: true
+    }
+  }
+})
