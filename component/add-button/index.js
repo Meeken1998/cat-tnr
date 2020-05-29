7c72f3ef9c60829ea2d6ee3e6c6250b9dee8f246
Meeken <78071836@qq.com>
2020-05-29 21:40:57 +0800

    fix

---
+++ component/add-button/index.js
@@ -0,0 +1,23 @@
+// component/add-button/index.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
